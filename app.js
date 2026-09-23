dc1fdfa18ed2ed9817c59032d191f3cd65a90b42
Abhijeet <abhijeetrai441@gmail.com>
2026-09-23 10:45:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,13 +49,11 @@ const gameDraw = () => {
   msgContainer.classList.remove("hide");
   disableBoxes();
 };
-
 const disableBoxes = () => {
   for (let box of boxes) {
     box.disabled = true;
   }
 };
-
 const enableBoxes = () => {
   for (let box of boxes) {
     box.disabled = false;
